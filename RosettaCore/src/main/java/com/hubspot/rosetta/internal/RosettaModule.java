6da3dfbb3a771a82fccd9e4b0d891487307e3486
Jonathan Haber <jhaber@hubspot.com>
2015-02-10 09:05:54 -0500

    Remove this

--- RosettaCore/src/main/java/com/hubspot/rosetta/internal/RosettaModule.java
+++ RosettaCore/src/main/java/com/hubspot/rosetta/internal/RosettaModule.java
@@ -1,9 +1,7 @@
 package com.hubspot.rosetta.internal;
 
-import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SuppressWarnings("serial")
 public class RosettaModule extends Module {
@@ -22,11 +20,5 @@ public class RosettaModule extends Module {
   public void setupModule(SetupContext context) {
     context.insertAnnotationIntrospector(new RosettaAnnotationIntrospector());
     context.addBeanSerializerModifier(new StoredAsJsonBeanSerializerModifier());
-
-    ObjectCodec codec = context.getOwner();
-    if (codec instanceof ObjectMapper) {
-      ObjectMapper mapper = (ObjectMapper) codec;
-      mapper.setSerializerProvider(new NoViewSerializerProvider());
-    }
   }
 }
