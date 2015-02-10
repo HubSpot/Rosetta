26ee20805f40f5fe4ce1bdbe2d9119d2ef49fa43
Jonathan Haber <jhaber@hubspot.com>
2015-02-10 09:09:45 -0500

    Use default serializer provider

--- RosettaCore/src/main/java/com/hubspot/rosetta/internal/RosettaModule.java
+++ RosettaCore/src/main/java/com/hubspot/rosetta/internal/RosettaModule.java
@@ -1,7 +1,10 @@
 package com.hubspot.rosetta.internal;
 
+import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
 
 @SuppressWarnings("serial")
 public class RosettaModule extends Module {
@@ -20,5 +23,11 @@ public class RosettaModule extends Module {
   public void setupModule(SetupContext context) {
     context.insertAnnotationIntrospector(new RosettaAnnotationIntrospector());
     context.addBeanSerializerModifier(new StoredAsJsonBeanSerializerModifier());
+
+    ObjectCodec codec = context.getOwner();
+    if (codec instanceof ObjectMapper) {
+      ObjectMapper mapper = (ObjectMapper) codec;
+      mapper.setSerializerProvider(new DefaultSerializerProvider.Impl());
+    }
   }
 }
