a48cfa0c6186ea40cb4de843a0c1a4d7133c86f7
ldriscoll <ldriscoll@hubspot.com>
2015-04-21 14:20:33 -0400

    Add a default value for the RosettaProperty, it has become clear that this can be used in conjunction with an @JsonIgnore so that rosetta uses the value, when jackson does not.

--- RosettaCore/src/test/java/com/hubspot/rosetta/beans/RosettaPropertyBean.java
+++ RosettaCore/src/test/java/com/hubspot/rosetta/beans/RosettaPropertyBean.java
@@ -1,10 +1,15 @@
 package com.hubspot.rosetta.beans;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hubspot.rosetta.annotations.RosettaProperty;
 
 public class RosettaPropertyBean {
   @RosettaProperty("mccartney_song_title")
   private String mcCartneySongTitle;
+
+  @RosettaProperty
+  @JsonIgnore
+  private String jsonIgnoreRosettaUse;
 
   public String getMcCartneySongTitle() {
     return mcCartneySongTitle;
@@ -12,6 +17,15 @@ public class RosettaPropertyBean {
 
   public RosettaPropertyBean setMcCartneySongTitle(String mcCartneySongTitle) {
     this.mcCartneySongTitle = mcCartneySongTitle;
+    return this;
+  }
+
+  public String getJsonIgnoreRosettaUse() {
+    return jsonIgnoreRosettaUse;
+  }
+
+  public RosettaPropertyBean setJsonIgnoreRosettaUse(String jsonIgnoreRosettaUse) {
+    this.jsonIgnoreRosettaUse = jsonIgnoreRosettaUse;
     return this;
   }
 }
