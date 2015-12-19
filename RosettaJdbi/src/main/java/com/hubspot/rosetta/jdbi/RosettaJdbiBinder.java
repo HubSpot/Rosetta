3bab8313d82746088199f339147eaaa02a0cf9ae
Jonathan Haber <jhaber@hubspot.com>
2015-12-18 22:57:08 -0500

    Add test for list behavior

--- RosettaJdbi/src/main/java/com/hubspot/rosetta/jdbi/RosettaJdbiBinder.java
+++ RosettaJdbi/src/main/java/com/hubspot/rosetta/jdbi/RosettaJdbiBinder.java
@@ -6,14 +6,7 @@ import com.hubspot.rosetta.RosettaBinder.Callback;
 import org.skife.jdbi.v2.SQLStatement;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.skife.jdbi.v2.StatementContext;
 import org.skife.jdbi.v2.sqlobject.Binder;
-import org.skife.jdbi.v2.tweak.Argument;
-
-import java.sql.Array;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
 
 public enum RosettaJdbiBinder implements Binder<BindWithRosetta, Object> {
   INSTANCE;
@@ -34,19 +27,7 @@ public enum RosettaJdbiBinder implements Binder<BindWithRosetta, Object> {
 
       @Override
       public void bind(String key, final Object value) {
-        if (value instanceof List) {
-          q.bind(key, new Argument() {
-
-            @Override
-            public void apply(int position, PreparedStatement statement, StatementContext ctx) throws SQLException {
-              // TODO don't hardcode VARCHAR
-              Array array = ctx.getConnection().createArrayOf("VARCHAR", ((List<?>) value).toArray());
-              statement.setArray(position, array);
-            }
-          });
-        } else{
-          q.bind(key, value);
-        }
+        q.bind(key, value);
       }
     });
   }
