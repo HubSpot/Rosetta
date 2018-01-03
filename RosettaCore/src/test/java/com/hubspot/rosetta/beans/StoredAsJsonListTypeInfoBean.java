90b30d90a91abb78933dd97243a00afc531eecc2
ldriscoll <ldriscoll@hubspot.com>
2018-01-03 17:38:13 -0500

    space.

--- RosettaCore/src/test/java/com/hubspot/rosetta/beans/StoredAsJsonListTypeInfoBean.java
+++ RosettaCore/src/test/java/com/hubspot/rosetta/beans/StoredAsJsonListTypeInfoBean.java
@@ -14,7 +14,6 @@ import com.hubspot.rosetta.beans.StoredAsJsonListTypeInfoBean.ConcreteStoredAsJs
     @JsonSubTypes.Type(value = ConcreteStoredAsJsonList.class)
 })
 public abstract class StoredAsJsonListTypeInfoBean {
-
   @StoredAsJson
   public abstract List<InnerBean> getInnerBeans();
 
