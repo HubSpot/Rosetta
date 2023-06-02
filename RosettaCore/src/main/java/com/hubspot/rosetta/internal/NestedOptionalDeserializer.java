45f9f0705ccc8c3a702a8fc8d148a939d11fd796
Przemyslaw Gliniecki <pgliniecki@hubspot.com>
2023-06-02 13:33:47 +0100

    StdScalarDeserializer -> StdDeserializer

--- RosettaCore/src/main/java/com/hubspot/rosetta/internal/NestedOptionalDeserializer.java
+++ RosettaCore/src/main/java/com/hubspot/rosetta/internal/NestedOptionalDeserializer.java
@@ -9,9 +9,9 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
-public class NestedOptionalDeserializer<T> extends StdScalarDeserializer<Optional<T>> {
+public class NestedOptionalDeserializer<T> extends StdDeserializer<Optional<T>> {
 
   private final Class<T> referencedClazz;
 
