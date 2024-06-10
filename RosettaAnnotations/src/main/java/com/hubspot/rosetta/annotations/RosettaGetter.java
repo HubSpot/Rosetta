d8a67cddcb6eaa26c31b07d0a9ae21b7a5cb0d4f
skudikala <skudikala@hubspot.com>
2024-06-10 17:12:30 -0400

    update docs

--- RosettaAnnotations/src/main/java/com/hubspot/rosetta/annotations/RosettaGetter.java
+++ RosettaAnnotations/src/main/java/com/hubspot/rosetta/annotations/RosettaGetter.java
@@ -6,12 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * like @JsonProperty only limited to Rosetta mapping/binding
- *
- * For instance, if you have a property that is mcCartneySongTitle, and are using
- * a naming strategy of snake case, then the field would be expected to be mc_cartney_song_title,
- * but you might want it to be mccartney_song_title without changing the way that
- * jackson serializes/deserializes when sending through services.
+ * like @JsonGetter only limited to Rosetta mapping/binding
  */
 @Target(
   {
