662ad6a4e8f990d500df3c5c8cec4201d7b79e67
skudikala <skudikala@hubspot.com>
2024-06-10 14:32:41 -0400

    looks like the inherited annotation is not inherited

--- RosettaAnnotations/src/main/java/com/hubspot/rosetta/annotations/RosettaProperty.java
+++ RosettaAnnotations/src/main/java/com/hubspot/rosetta/annotations/RosettaProperty.java
@@ -1,6 +1,7 @@
 package com.hubspot.rosetta.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -23,6 +24,7 @@ import java.lang.annotation.Target;
 )
 @Retention(RetentionPolicy.RUNTIME)
 @RosettaAnnotation
+@Inherited
 public @interface RosettaProperty {
   String USE_DEFAULT_NAME = "";
 
